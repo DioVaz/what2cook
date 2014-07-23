c7acd2e1f740911e0b08d560c29daf1e2fb778ea
DioVaz <snowblade90@gmail.com>
2014-07-23 23:02:23 +0100

    base de visualisação de receitas melhorada
    ainda falta editar
    corrigir bug de comprimento da preparação e ingredientes alterando a
    template.

--- js/app.js
+++ js/app.js
@@ -16,14 +16,15 @@ var posts = [{
 	id: '1',
 	title: "Arroz de marisco",
 	author: {name: "Diogo Vaz"},
-	date: new Date('19-07-2014'),
-	description:"Um belo prato para receber amigos em casa",
-	ingredients:"Cebola: 1, Alho: 4 dentes,  Azeite: 4 colheres de sopa,  Tomate: 4,  Sal: q.b., Marisco: 1 embalagem (mistura),  Caldo de peixe: 1 cubo,  Arroz: 1 chávena,  Coentros: q.b.,  piri-piri: q.b."
+	date: new Date('07-19-2014'),
+	ingredients:"Cebola: 1, Alho: 4 dentes,  Azeite: 4 colheres de sopa,  Tomate: 4,  Sal: q.b., Marisco: 1 embalagem (mistura),  Caldo de peixe: 1 cubo,  Arroz: 1 chávena,  Coentros: q.b.,  piri-piri: q.b.",
+	description:"Refogue no azeite a cebola grande e os dentes de alho bem picados.  Sirva de seguida."
 }, {
 	id: '2',
 	title: "Iscas de bacalhau",
 	author: {name: "Diogo Vaz"},
-	date: new Date('21-07-2014'),
-	description:"O tradicional na sua mesa",
-	ingredients:"Cebola: 1,  Alho: 4 dentes,  Azeite: 4 colheres de sopa,  Tomate: 4,  Sal: q.b., Marisco: 1 embalagem (mistura),  Caldo de peixe: 1 cubo,  Arroz: 1 chávena,  Coentros: q.b.,  piri-piri: q.b."
+	date: new Date('07-20-2014'),
+	ingredients:"Cebola: 1,  Alho: 4 dentes,  Azeite: 4 colheres de sopa,  Tomate: 4,  Sal: q.b., Marisco: 1 embalagem (mistura),  Caldo de peixe: 1 cubo,  Arroz: 1 chávena,  Coentros: q.b.,  piri-piri: q.b.",
+	description:"O tradicional na sua mesa"
+	
 }];
